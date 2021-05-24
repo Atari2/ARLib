b55354eda69fd893a7db7128c42395533d0e7611
Alessio <alex.robassomero@gmail.com>
2021-05-24 14:36:00 +0200

    Fixed compilation on GCC v3

--- cstring_compat.h
+++ cstring_compat.h
@@ -1,12 +1,19 @@
 #pragma once
 #include "Types.h"
-size_t strlen(const char*);
-char* strcpy(char* dest, const char* src);
-char* strncpy(char* destination, const char* source, size_t num);
-int strcmp(const char*, const char*);
-int strncmp(const char*, const char*, size_t);
-char* strcat(char* dst, const char* src);
-int isspace(int c);
-void* memmove(void* dst, const void* src, size_t num);
-int toupper(int c);
-int tolower(int c);
+namespace ARLib {
+	constexpr inline size_t strlen(const char* ptr) {
+		size_t len = 0;
+		while (*(ptr++))
+			len++;
+		return len;
+	}
+	char* strcpy(char* dest, const char* src);
+	char* strncpy(char* destination, const char* source, size_t num);
+	int strcmp(const char*, const char*);
+	int strncmp(const char*, const char*, size_t);
+	char* strcat(char* dst, const char* src);
+	int isspace(int c);
+	void* memmove(void* dst, const void* src, size_t num);
+	int toupper(int c);
+	int tolower(int c);
+}
