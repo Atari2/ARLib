01d00a448073f11544a36159cb2fc7fb3c940af5
Alessio <alex.robassomero@gmail.com>
2021-05-28 01:35:16 +0200

    Implemented sqrt()

---
+++ cmath_compat.cpp
@@ -0,0 +1,25 @@
+#include "cmath_compat.h"
+#ifdef _MSC_VER
+#include <emmintrin.h>
+#endif
+namespace ARLib {
+	double sqrt(double arg)
+	{
+#ifdef _MSC_VER
+		alignas(16) double mem_addr[]{ 0, 0 };
+		_mm_store_pd1(mem_addr, _mm_sqrt_pd(_mm_load1_pd(&arg)));
+		return mem_addr[0];
+#else
+		double b = 0.0;
+		__asm__ volatile(
+			"movq %1, %%xmm0 \n"
+			"sqrtsd %%xmm0, %%xmm1 \n"
+			"movq %%xmm1, %0 \n"
+			: "=r"(b)
+			: "g"(arg)
+			: "xmm0", "xmm1", "memory"
+			);
+		return b;
+#endif
+	}
+}
---
+++ cmath_compat.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "Types.h"
+namespace ARLib {
+	double sqrt(double arg);
+}
