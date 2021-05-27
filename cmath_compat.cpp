715cd4079e335752a130ad60dcf9bffb0d8b1678
Alessio <alex.robassomero@gmail.com>
2021-05-28 01:47:42 +0200

    Avoiding using a macro in the sqrt function (MSVC)

--- cmath_compat.cpp
+++ cmath_compat.cpp
@@ -7,7 +7,7 @@ namespace ARLib {
 	{
 #ifdef _MSC_VER
 		alignas(16) double mem_addr[]{ 0, 0 };
-		_mm_store_pd1(mem_addr, _mm_sqrt_pd(_mm_load1_pd(&arg)));
+		_mm_store1_pd(mem_addr, _mm_sqrt_pd(_mm_load1_pd(&arg)));
 		return mem_addr[0];
 #else
 		double b = 0.0;
