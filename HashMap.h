7f99eae3a9339408ffda01c470e18b921854fa51
Alessio <alex.robassomero@gmail.com>
2021-06-01 01:22:42 +0200

    Slightly reworked HashMap, now it replaces instead of inserting if entry is already there

--- HashMap.h
+++ HashMap.h
@@ -2,41 +2,89 @@
 #include "HashBase.h"
 #include "Concepts.h"
 #include "Assertion.h"
+#include "HashTable.h"
 
 namespace ARLib {
+
+	enum class InsertionResult {
+		New,
+		Replace
+	};
+
 	template <Hashable Key, EqualityComparable Val>
 	class HashMap {
 		struct Entry {
-			Key key;
-			Val value;
+			Key m_key;
+			Val m_value;
+		public:
+			Entry(Key key, Val value) : m_key(move(key)), m_value(move(value)) {
+
+			}
+			Entry(Entry&& other) noexcept : m_key(move(other.m_key)), m_value(move(other.m_value)) {
+
+			}
+			Entry(const Entry& other) : m_key(other.m_key), m_value(other.m_value) {
+
+			}
+
+			Entry& operator=(Entry&& other) noexcept {
+				m_key = move(other.m_key);
+				m_value = move(other.m_value);
+				return *this;
+			}
+			bool operator==(const Entry& other) {
+				return hash_equals(*this, other);
+			}
+
+			bool operator!=(const Entry& other) {
+				return !hash_equals(*this, other);
+			}
+
+			friend uint32_t hash(const Entry& s) {
+				return hash(s.m_key);
+			}
+			friend bool hash_equals(const Entry& a, const Entry& b) {
+				return hash(a) == hash(b);
+			}
 		};
+
 		size_t m_size = 0;
-		size_t m_capacity = 0;
 
-		void resize_to_capacity_(size_t capacity) {
-			m_capacity = capacity;
-		}
+		HashTable<Entry> m_table{};
 	public:
-		TODO(HashMap)
-		// HashMap() = default;
-		/*
-		HashMap(std::initializer_list<Entry> list) {
-			resize_to_capacity_(list.size());
-			for (auto& item : list)
-				add(item.key, item.value);
+		HashMap() = default;
+
+		InsertionResult add(Key key, Val value) {
+			Entry entry{ key, value };
+			uint32_t hs = hash(entry);
+			auto iter = m_table.find_uncertain_precalc(entry, hs);
+			if (iter == m_table.end_precalc(hs)) {
+				m_table.insert(Forward<Entry>(entry));
+				m_size++;
+				return InsertionResult::New;
+			}
+			else {
+				(*iter) = move(entry);
+				return InsertionResult::Replace;
+			}
 		}
-		*/
-
-		void add(Key key, Val value) {
-
-		}
-		void add(Entry entry) {
-
+		InsertionResult add(Entry entry) {
+			uint32_t hs = hash(entry);
+			auto iter = m_table.find_uncertain_precalc(entry, hs);
+			if (iter == m_table.end_precalc(hs)) {
+				m_table.insert(Forward<Entry>(entry));
+				m_size++;
+				return InsertionResult::New;
+			}
+			else {
+				(*iter) = move(entry);
+				return InsertionResult::Replace;
+			}
 		}
 
 		size_t size() const { return m_size; }
-		size_t capacity() const { return m_capacity; }
 	};
 }
 
 using ARLib::HashMap;
+using ARLib::InsertionResult;
