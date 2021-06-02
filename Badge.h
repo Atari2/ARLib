8366b23cf63f80c752d40e784f16d8785fb266d1
Alessio <alex.robassomero@gmail.com>
2021-06-02 13:19:22 +0200

    Added Badge class

---
+++ Badge.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace ARLib {
+	template <typename T>
+	class Badge {
+		friend T;
+		constexpr Badge() = default;
+		constexpr Badge(const Badge&) = default;
+		constexpr Badge(Badge&&) = default;
+		constexpr Badge<T>& operator=(const Badge<T>&) = default;
+		constexpr Badge<T>& operator=(Badge<T>&&) = default;
+	};
+}
