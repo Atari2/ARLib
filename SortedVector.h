320e699c9f8b41487e5c087a4f99d816c0284c8d
Alessio <alex.robassomero@gmail.com>
2021-06-16 00:20:28 +0200

    Add a WIP SortedVector implementation (it's not very efficient)

---
+++ SortedVector.h
@@ -0,0 +1,197 @@
+#pragma once
+#include "Ordering.h"
+#include "Compat.h"
+#include "Utility.h"
+#include "cstring_compat.h"
+#include "Algorithm.h"
+#include "Iterator.h"
+#include "Assertion.h"
+
+namespace ARLib {
+
+	template <typename T>
+	struct DefaultOrdering {
+		forceinline Ordering operator()(const T& first, const T& second) {
+			return first <=> second;
+		}
+	};
+
+	template <typename T, class Ordering = DefaultOrdering<T>>
+	class SortedVector {
+			
+		using Iter = Iterator<T>;
+		using ConstIter = ConstIterator<T>;
+		using ReverseIter = ReverseIterator<T>;
+		using ConstReverseIter = ConstReverseIterator<T>;
+
+		static constexpr inline auto npos = static_cast<size_t>(-1);
+		Ordering m_ordering{};
+		T* m_storage = nullptr;
+		size_t m_size = 0;
+		size_t m_capacity = 0;
+
+		void grow_to_capacity_(size_t capacity) {
+			T* new_storage = new T[capacity];
+			if constexpr (IsTriviallyCopiableV<T>) {
+				memmove(new_storage, m_storage, sizeof(T) * m_size);
+			}
+			else {
+				for (size_t i = 0; i < m_size; i++) {
+					new_storage[i] = move(m_storage[i]);
+				}
+			}
+			delete[] m_storage;
+			m_storage = new_storage;
+			m_capacity = capacity;
+		}
+
+		void ensure_capacity_() {
+			if (m_size == m_capacity) {
+				if constexpr (IsTriviallyCopiableV<T>) {
+					grow_to_capacity_(basic_growth(m_capacity));
+				}
+				else {
+					grow_to_capacity_(bit_round_growth(m_capacity));
+				}
+			}
+		}
+
+		void insert_single_element_(T&& element) {
+			size_t insert_index = 0;
+			// this algo can stand improvement (e.g. using a simple binary search instead of linear)
+			while (insert_index < m_size && m_ordering(m_storage[insert_index], element) == less)
+				insert_index++;
+			ensure_capacity_();
+			if constexpr (IsTriviallyCopiableV<T>) {
+				memmove(m_storage + insert_index + 1, m_storage + insert_index, sizeof(T) * (m_size - insert_index));
+			}
+			else {
+				for (size_t i = m_size; i > insert_index; i--) {
+					m_storage[i] = move(m_storage[i - 1]);
+				}
+			}
+			m_storage[insert_index] = move(element);
+			m_size++;
+		}
+	public:
+		SortedVector() = default;
+		SortedVector(size_t capacity) : m_capacity(capacity) {
+			m_storage = new T[m_capacity];
+		}
+		SortedVector(std::initializer_list<T> lst) {
+			grow_to_capacity_(lst.size());
+			for (auto elem : lst)
+				insert_single_element_(Forward<T>(elem));
+		}
+
+		template <typename Functor>
+		void for_each(Functor func) {
+			for (const auto& item : *this)
+				func(item);
+		}
+
+		void remove(size_t index) {
+			SOFT_ASSERT_FMT((index < m_size), "Index %lu was out of bounds in vector of size %lu", index, m_size);
+			m_storage[index].~T();
+			m_size--;
+			if constexpr (IsTriviallyCopiableV<T>) {
+				memmove(m_storage + index, m_storage + index + 1, sizeof(T) * (m_size - index));
+			}
+			else {
+				for (size_t i = index; i < m_size; i++)
+					m_storage[i] = move(m_storage[i + 1]);
+			}
+		}
+
+		void remove(const T& val) {
+			remove(find(val));
+		}
+
+		size_t find(const T& element) {
+			size_t left = 0;
+			size_t right = m_size - 1;
+			if (m_ordering(element, m_storage[left]) == less || m_ordering(element, m_storage[right]) == greater)
+				return npos;
+			while (left <= right) {
+				size_t mid = (left + right) / 2;
+				auto ord = m_ordering(m_storage[mid], element);
+				if (ord == equal)
+					return mid;
+				else if (ord == greater)
+					right = mid - 1;
+				else
+					left = mid + 1;
+			}
+			return npos;
+		}
+		void insert(T element) {
+			insert_single_element_(Forward<T>(element));
+		}
+		const T& operator[](size_t index) const {
+			return m_storage[index];
+		}
+		T& operator[](size_t index) {
+			return m_storage[index];
+		}
+		T* data() {
+			return m_storage;
+		}
+		void clear() {
+			m_size = 0;
+			delete[] m_storage;
+			m_storage = nullptr;
+			m_capacity = 0;
+		}
+		bool empty() const {
+			return m_size == 0;
+		}
+		size_t size() const {
+			return m_size;
+		}
+		size_t capacity() const {
+			return m_capacity;
+		}
+		const Ordering& ordering() const {
+			return m_ordering;
+		}
+
+		Iter begin() const {
+			return { m_storage };
+		}
+
+		Iter end() const {
+			return { m_storage + m_size };
+		}
+
+		ConstIter cbegin() const {
+			return { m_storage };
+		}
+
+		ConstIter cend() const {
+			return { m_storage + m_size };
+		}
+
+		ReverseIter rbegin() const {
+			return { m_storage + m_size - 1 };
+		}
+
+		ReverseIter rend() const {
+			return { m_storage - 1 };
+		}
+
+		ConstReverseIter crbegin() const {
+			return { m_storage + m_size - 1 };
+		}
+
+		ConstReverseIter crend() const {
+			return { m_storage - 1 };
+		}
+
+		~SortedVector() {
+			if (m_capacity)
+				delete[] m_storage;
+		}
+	};
+}
+
+using ARLib::SortedVector;
