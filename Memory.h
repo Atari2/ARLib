711c42365ed21a95e4afeabfaa39258b4d97c643
Alessio <alex.robassomero@gmail.com>
2021-07-09 12:48:32 +0200

    Added ConditionalBitMove and ConditionalBitCopy.

--- Memory.h
+++ Memory.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "TypeTraits.h"
+#include "cstring_compat.h"
+#include "Concepts.h"
 
 namespace ARLib {
     template <class To, class From,
@@ -8,6 +10,34 @@ namespace ARLib {
                         int> = 0>
     [[nodiscard]] constexpr To BitCast(const From& val) noexcept {
         return __builtin_bit_cast(To, val);
+    }
+
+    // dst and src may not overlap
+    template <CopyAssignable T>
+    void ConditionalBitCopy(T* dst, const T* src, size_t count) {
+        if (!dst || !src || count == 0) return;
+        if constexpr (IsTriviallyCopiableV<T>) {
+            memcpy(dst, src, count * sizeof(T));
+        } else {
+            for (size_t i = 0; i < count; i++) {
+                dst[i] = src[i];
+            }
+        }
+    }
+
+    // dst and src may not overlap
+    template <MoveAssignable T>
+    void ConditionalBitMove(T* dst, T* src, size_t count) {
+        if (!dst || !src || count == 0) return;
+        if constexpr (IsTriviallyCopiableV<T>) {
+            memcpy(dst, src, count * sizeof(T));
+            delete[] src;
+            src = nullptr;
+        } else {
+            for (size_t i = 0; i < count; i++) {
+                dst[i] = move(src[i]);
+            }
+        }
     }
 } // namespace ARLib
 
