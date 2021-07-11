7c58dbdb7f3666cccad1ad9c3ab301cd05f69f78
Alessio <alex.robassomero@gmail.com>
2021-07-11 20:02:39 +0200

    Added equality operator to Tuple

--- Tuple.h
+++ Tuple.h
@@ -7,6 +7,19 @@ namespace ARLib {
     class Tuple : Tuple<Args...> {
         T m_member;
 
+        template <size_t N>
+        bool equality_impl(const Tuple& other) const {
+            if constexpr (N == 0)
+                return m_member == other.m_member;
+            else {
+                if (this->template get<N>() == other.template get<N>()) {
+                    return equality_impl<N - 1>(other);
+                } else {
+                    return false;
+                }
+            }
+        }
+
         public:
         Tuple() = default;
         Tuple(const Tuple&) = default;
@@ -16,6 +29,18 @@ namespace ARLib {
         Tuple& operator=(const Tuple&) = default;
         Tuple& operator=(Tuple&&) = default;
 
+        bool operator==(const Tuple& other) const { return equality_impl<sizeof...(Args) - 1>(other); }
+
+        template <typename Tp>
+        constexpr const Tp& get_typed() const {
+            static_assert(IsAnyOfV<Tp, T, Args...>);
+            if constexpr (SameAs<Tp, T>) {
+                return m_member;
+            } else {
+                return static_cast<const Tuple<Args...>*>(this)->template get_typed<Tp>();
+            }
+        }
+
         template <typename Tp>
         constexpr Tp& get_typed() {
             static_assert(IsAnyOfV<Tp, T, Args...>);
@@ -23,6 +48,16 @@ namespace ARLib {
                 return m_member;
             } else {
                 return static_cast<Tuple<Args...>*>(this)->template get_typed<Tp>();
+            }
+        }
+
+        template <size_t N>
+        constexpr const auto& get() const {
+            if constexpr (N == 0) {
+                return m_member;
+            } else {
+                static_assert(N < (sizeof...(Args) + 1) && N > 0);
+                return static_cast<const Tuple<Args...>*>(this)->template get<N - 1>();
             }
         }
 
@@ -65,14 +100,28 @@ namespace ARLib {
         Tuple() = default;
         Tuple(T arg) : m_member(move(arg)) {}
 
+        bool operator==(const Tuple& other) const { return m_member == other.m_member; }
+
         template <typename Tp>
         T& get_typed() {
             static_assert(SameAs<Tp, T>);
             return m_member;
         }
 
+        template <typename Tp>
+        const T& get_typed() const {
+            static_assert(SameAs<Tp, T>);
+            return m_member;
+        }
+
         template <size_t N>
         constexpr T& get() {
+            static_assert(N == 0);
+            return m_member;
+        }
+
+        template <size_t N>
+        constexpr const T& get() const {
             static_assert(N == 0);
             return m_member;
         }
