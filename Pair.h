dae6e864e6627b5f6fc251936125dea765c2ca98
Alessio <alex.robassomero@gmail.com>
2021-07-17 00:20:33 +0200

    Added Pair specialization for reference types.

--- Pair.h
+++ Pair.h
@@ -53,6 +53,52 @@ namespace ARLib {
         ~Pair() = default;
     };
 
+    template <typename T, typename U>
+    struct Pair<T&, U&> {
+        T& m_first;
+        U& m_second;
+
+        Pair(T& first, U& second) : m_first(first), m_second(second) {}
+
+        Pair(const Pair& other) : m_first(other.m_first), m_second(other.m_second){};
+        Pair(Pair&& other) : m_first(other.m_first), m_second(other.m_second){};
+
+        T& first() { return m_first; }
+        U& second() { return m_second; }
+
+        const T& first() const { return m_first; }
+        const U& second() const { return m_second; }
+
+        template <size_t Index>
+        auto& get() & {
+            static_assert(Index == 0 || Index == 1);
+            if constexpr (Index == 0)
+                return m_first;
+            else if constexpr (Index == 1)
+                return m_second;
+        }
+
+        template <size_t Index>
+        auto const& get() const& {
+            static_assert(Index == 0 || Index == 1);
+            if constexpr (Index == 0)
+                return m_first;
+            else if constexpr (Index == 1)
+                return m_second;
+        }
+
+        template <size_t Index>
+        auto&& get() && {
+            static_assert(Index == 0 || Index == 1);
+            if constexpr (Index == 0)
+                return move(m_first);
+            else if constexpr (Index == 1)
+                return move(m_second);
+        }
+
+        ~Pair() = default;
+    };
+
 } // namespace ARLib
 
 using ARLib::Pair;
