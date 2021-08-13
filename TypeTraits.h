fbed94ea525108497289a0216fca91028e0af657
Alessio <alex.robassomero@gmail.com>
2021-08-13 18:51:25 +0200

    Don't include Utility in type traits, added a bunch of function related
    traits

--- TypeTraits.h
+++ TypeTraits.h
@@ -1,8 +1,95 @@
 #pragma once
 #include "BaseTraits.h"
-#include "Utility.h"
 
 namespace ARLib {
+
+    template <class T>
+    inline constexpr T&& ForwardTrait(typename RemoveReference<T>::type& t) noexcept {
+        return static_cast<T&&>(t);
+    }
+
+    template <class T>
+    inline constexpr T&& ForwardTrait(typename RemoveReference<T>::type&& t) noexcept {
+        return static_cast<T&&>(t);
+    }
+
+    template <typename P>
+    struct Not : public BoolConstant<!bool(P::value)> {};
+
+    template <typename...>
+    struct Or;
+
+    template <>
+    struct Or<> : public FalseType {};
+
+    template <typename B1>
+    struct Or<B1> : public B1 {};
+
+    template <typename B1, typename B2>
+    struct Or<B1, B2> : public Conditional<B1::value, B1, B2>::type {};
+
+    template <typename B1, typename B2, typename B3, typename... Bn>
+    struct Or<B1, B2, B3, Bn...> : public Conditional<B1::value, B1, Or<B2, B3, Bn...>>::type {};
+
+    template <typename T>
+    struct IsObject : public Not<Or<IsFunction<T>, IsReference<T>, IsVoid<T>>>::type {};
+
+    template <typename U, bool IsArray = IsArray<U>::value, bool IsFunction = IsFunction<U>::value>
+    struct DecaySelector;
+
+    template <typename U>
+    struct DecaySelector<U, false, false> {
+        typedef typename RemoveCv<U>::type type;
+    };
+
+    template <typename U>
+    struct DecaySelector<U, true, false> {
+        typedef typename RemoveExtent<U>::type* type;
+    };
+
+    template <typename U>
+    struct DecaySelector<U, false, true> {
+        typedef typename AddPointer<U>::type type;
+    };
+
+    template <typename T>
+    class Decay {
+        typedef typename RemoveReference<T>::type remove_type;
+
+        public:
+        typedef typename DecaySelector<remove_type>::type type;
+    };
+
+    template <typename T>
+    using RemoveCvRefT = typename RemoveCv<typename RemoveReference<T>::type>::type;
+
+    template <typename T, typename U = RemoveCvRefT<T>>
+    struct InvUnwrap {
+        using type = T;
+    };
+
+    template <typename T, typename U>
+    struct InvUnwrap<T, detail::ReferenceWrapper<U>> {
+        using type = U&;
+    };
+
+        template <typename>
+    struct IsMemberObjectPointerHelper : public FalseType {};
+
+    template <typename T, typename C>
+    struct IsMemberObjectPointerHelper<T C::*> : public Not<IsFunction<T>>::type {};
+
+    template <typename T>
+    struct IsMemberObjectPointer : public IsMemberObjectPointerHelper<typename RemoveCv<T>::type>::type {};
+
+    template <typename>
+    struct IsMemberFunctionPointerHelper : public FalseType {};
+
+    template <typename T, typename C>
+    struct IsMemberFunctionPointerHelper<T C::*> : public IsFunction<T>::type {};
+
+    template <typename T>
+    struct IsMemberFunctionPointer : public IsMemberFunctionPointerHelper<typename RemoveCv<T>::type>::type {};
 
     template <typename Ptr>
     struct PointerTraits {
@@ -63,8 +150,8 @@ namespace ARLib {
     // convertible
     template <class From, class To>
     struct Convertible :
-        IntegralConstant<bool, (decltype(detail::TestReturnable<To>(0))::value&& decltype(
-                               detail::TestImplicitlyConvertible<From, To>(0))::value) ||
+        IntegralConstant<bool, (decltype(detail::TestReturnable<To>(
+                               0))::value&& decltype(detail::TestImplicitlyConvertible<From, To>(0))::value) ||
                                (IsVoid<From>::value && IsVoid<To>::value)> {};
 
     template <class From, class To>
@@ -191,23 +278,12 @@ namespace ARLib {
     template <class T>
     using MakeUnsignedT = typename MakeUnsigned<T>::type;
 
-    template <class T>
-    struct Decay {
-        private:
-        typedef typename RemoveReference<T>::type U;
-
-        public:
-        typedef typename Conditional<IsArray<U>::value, typename RemoveExtent<U>::type*,
-                                     typename Conditional<IsFunction<U>::value, typename AddPointer<U>::type,
-                                                          typename RemoveCv<U>::type>::type>::type type;
-    };
-
     namespace detail {
 
         template <class T>
         struct InvokeImpl {
             template <class F, class... Args>
-            static auto call(F&& f, Args&&... args) -> decltype(Forward<F>(f)(Forward<Args>(args)...));
+            static auto call(F&& f, Args&&... args) -> decltype(ForwardTrait<F>(f)(ForwardTrait<Args>(args)...));
         };
 
         template <class B, class MT>
@@ -223,18 +299,19 @@ namespace ARLib {
             template <class T, class Td = typename Decay<T>::type,
                       class = typename EnableIf<!BaseOf<B, Td>::value>::type,
                       class = typename EnableIf<!ReferenceWrapper<Td>::value>::type>
-            static auto get(T&& t) -> decltype(*Forward<T>(t));
+            static auto get(T&& t) -> decltype(*ForwardTrait<T>(t));
 
             template <class T, class... Args, class MT1, class = typename EnableIf<IsFunction<MT1>::value>::type>
             static auto call(MT1 B::*pmf, T&& t, Args&&... args)
-            -> decltype((InvokeImpl::get(Forward<T>(t)).*pmf)(Forward<Args>(args)...));
+            -> decltype((InvokeImpl::get(ForwardTrait<T>(t)).*pmf)(ForwardTrait<Args>(args)...));
 
             template <class T>
-            static auto call(MT B::*pmd, T&& t) -> decltype(InvokeImpl::get(Forward<T>(t)).*pmd);
+            static auto call(MT B::*pmd, T&& t) -> decltype(InvokeImpl::get(ForwardTrait<T>(t)).*pmd);
         };
 
         template <class F, class... Args, class Fd = typename Decay<F>::type>
-        auto INVOKE(F&& f, Args&&... args) -> decltype(InvokeImpl<Fd>::call(Forward<F>(f), Forward<Args>(args)...));
+        auto INVOKE(F&& f, Args&&... args)
+        -> decltype(InvokeImpl<Fd>::call(ForwardTrait<F>(f), ForwardTrait<Args>(args)...));
 
         template <typename AlwaysVoid, typename, typename...>
         struct InvokeResult {};
@@ -242,11 +319,165 @@ namespace ARLib {
         struct InvokeResult<decltype(void(detail::INVOKE(declval<F>(), declval<Args>()...))), F, Args...> {
             using type = decltype(detail::INVOKE(declval<F>(), declval<Args>()...));
         };
+
+        template <typename T>
+        struct SuccessType {
+            typedef T type;
+        };
+
+        struct FailureType {};
+
+        struct InvokeOther {};
+        struct InvokeMemfunDeref {};
+        struct InvokeMemfunRef {};
+        struct InvokeMemobjDeref {};
+        struct InvokeMemobjRef {};
+
+        template <typename T, typename Tag>
+        struct ResultOfSuccess : SuccessType<T> {
+            using invoke_type = Tag;
+        };
+
+        struct ResultOfMemFunRefImpl {
+            template <typename F, typename T, typename... Args>
+            static ResultOfSuccess<decltype((declval<T>().*declval<F>())(declval<Args>()...)), InvokeMemfunRef>
+            s_test(int);
+
+            template <typename...>
+            static FailureType s_test(...);
+        };
+
+        template <typename MemPtr, typename Arg, typename... Args>
+        struct ResultOfMemFunRef : private ResultOfMemFunRefImpl {
+            typedef decltype(s_test<MemPtr, Arg, Args...>(0)) type;
+        };
+
+        struct ResultOfMemFunDerefImpl {
+            template <typename F, typename T, typename... Args>
+            static ResultOfSuccess<decltype(((*declval<T>()).*declval<F>())(declval<Args>()...)), InvokeMemfunDeref>
+            s_test(int);
+
+            template <typename...>
+            static FailureType s_test(...);
+        };
+
+        template <typename MemPtr, typename Arg, typename... Args>
+        struct ResultOfMemFunDeref : private ResultOfMemFunDerefImpl {
+            typedef decltype(s_test<MemPtr, Arg, Args...>(0)) type;
+        };
+
+        struct ResultOfMemObjRefImpl {
+            template <typename F, typename T>
+            static ResultOfSuccess<decltype(declval<T>().*declval<F>()), InvokeMemobjRef> s_test(int);
+
+            template <typename, typename>
+            static FailureType s_test(...);
+        };
+
+        template <typename MemPtr, typename Arg>
+        struct ResultOfMemObjRef : private ResultOfMemObjRefImpl {
+            typedef decltype(s_test<MemPtr, Arg>(0)) type;
+        };
+
+        struct ResultOfMemObjDerefImpl {
+            template <typename F, typename T>
+            static ResultOfSuccess<decltype((*declval<T>()).*declval<F>()), InvokeMemobjDeref> s_test(int);
+
+            template <typename, typename>
+            static FailureType s_test(...);
+        };
+
+        template <typename MemPtr, typename Arg>
+        struct ResultOfMemObjDeref : private ResultOfMemObjDerefImpl {
+            typedef decltype(s_test<MemPtr, Arg>(0)) type;
+        };
+
+        template <typename MemPtr, typename Arg>
+        struct ResultOfMemObj;
+
+        template <typename Res, typename Class, typename Arg>
+        struct ResultOfMemObj<Res Class::*, Arg> {
+            typedef RemoveCvRefT<Arg> Argval;
+            typedef Res Class::*Memptr;
+            typedef
+            typename Conditional<Or<IsSame<Argval, Class>, BaseOf<Class, Argval>>::value,
+                                 ResultOfMemObjRef<Memptr, Arg>, ResultOfMemObjDeref<Memptr, Arg>>::type::type type;
+        };
+
+        template <typename MemPtr, typename Arg, typename... Args>
+        struct ResultOfMemFun;
+
+        template <typename Res, typename Class, typename Arg, typename... Args>
+        struct ResultOfMemFun<Res Class::*, Arg, Args...> {
+            typedef typename RemoveReference<Arg>::type Argval;
+            typedef Res Class::*MemPtr;
+            typedef typename Conditional<BaseOf<Class, Argval>::value, ResultOfMemFunRef<MemPtr, Arg, Args...>,
+                                         ResultOfMemFunDeref<MemPtr, Arg, Args...>>::type::type type;
+        };
+
+        template <bool, bool, typename Functor, typename... Args>
+        struct ResultOfImpl {
+            typedef FailureType type;
+        };
+
+        template <typename MemPtr, typename Arg>
+        struct ResultOfImpl<true, false, MemPtr, Arg> :
+            public ResultOfMemObj<typename Decay<MemPtr>::type, typename InvUnwrap<Arg>::type> {};
+
+        template <typename MemPtr, typename Arg, typename... Args>
+        struct ResultOfImpl<false, true, MemPtr, Arg, Args...> :
+            public ResultOfMemFun<typename Decay<MemPtr>::type, typename InvUnwrap<Arg>::type, Args...> {};
+
+        struct ResultOfOtherImpl {
+            template <typename F, typename... Args>
+            static ResultOfSuccess<decltype(declval<F>()(std::declval<Args>()...)), InvokeOther> s_test(int);
+
+            template <typename...>
+            static FailureType s_test(...);
+        };
+
+        template <typename Functor, typename... Args>
+        struct ResultOfImpl<false, false, Functor, Args...> : private ResultOfOtherImpl {
+            typedef decltype(s_test<Functor, Args...>(0)) type;
+        };
+
+        template <typename Functor, typename... Args>
+        struct InvokeResultForFunc :
+            public ResultOfImpl<IsMemberObjectPointer<typename RemoveReference<Functor>::type>::value,
+                                IsMemberFunctionPointer<typename RemoveReference<Functor>::type>::value, Functor,
+                                Args...>::type {};
     } // namespace detail
 
     template <class T, bool = IsEnumV<T>>
     struct UnderlyingTypeImpl {
         using type = __underlying_type(T);
+    };
+
+    template <typename Result, typename Ret, bool = IsVoid<Ret>::value, typename = void>
+    struct IsInvokableImpl : FalseType {};
+
+    template <typename Result, typename Ret>
+    struct IsInvokableImpl<Result, Ret,
+                           /* IsVoid<Ret> = */ true, VoidT<typename Result::type>> : TrueType {};
+
+    template <typename Result, typename Ret>
+    struct IsInvokableImpl<Result, Ret,
+                           /* IsVoid<Ret> = */ false, VoidT<typename Result::type>> {
+        private:
+        static typename Result::type s_get();
+
+        template <typename T>
+        static void s_conv(T);
+
+        // This overload is viable if INVOKE(f, args...) can convert to _Tp.
+        template <typename T, typename = decltype(s_conv<T>(s_get()))>
+        static TrueType s_test(int);
+
+        template <typename T>
+        static FalseType s_test(...);
+
+        public:
+        using type = decltype(s_test<Ret>(1));
     };
 
     template <class T>
