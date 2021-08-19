733bb694d1a79e63538a83c60f347a62d85c6ba6
Alessio <alex.robassomero@gmail.com>
2021-08-19 18:10:03 +0200

    Add "ASSERTION FAILED: " to the assertion prints.

--- Assertion.h
+++ Assertion.h
@@ -49,14 +49,14 @@ namespace ARLib {
 
 #define HARD_ASSERT(val, msg)                                                                                          \
     if (!(val)) {                                                                                                      \
-        ARLib::puts(msg);                                                                                              \
+        ARLib::puts("ASSERTION FAILED: " msg);                                                                         \
         PRINT_SOURCE_LOCATION                                                                                          \
         assertion_failed__();                                                                                          \
         unreachable                                                                                                    \
     }
 #define HARD_ASSERT_FMT(val, fmt, ...)                                                                                 \
     if (!(val)) {                                                                                                      \
-        ARLib::printf(fmt "\n", __VA_ARGS__);                                                                          \
+        ARLib::printf("ASSERTION FAILED: " fmt "\n", __VA_ARGS__);                                                     \
         PRINT_SOURCE_LOCATION                                                                                          \
         assertion_failed__();                                                                                          \
         unreachable                                                                                                    \
@@ -64,12 +64,12 @@ namespace ARLib {
 
 #define SOFT_ASSERT(val, msg)                                                                                          \
     if (!(val)) {                                                                                                      \
-        ARLib::puts(msg);                                                                                              \
+        ARLib::puts("ASSERTION FAILED: " msg);                                                                         \
         PRINT_SOURCE_LOCATION                                                                                          \
     }
 #define SOFT_ASSERT_FMT(val, fmt, ...)                                                                                 \
     if (!(val)) {                                                                                                      \
-        ARLib::printf(fmt "\n", __VA_ARGS__);                                                                          \
+        ARLib::printf("ASSERTION FAILED: " fmt "\n", __VA_ARGS__);                                                     \
         PRINT_SOURCE_LOCATION                                                                                          \
     }
 
