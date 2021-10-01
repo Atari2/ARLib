d73288c760629de44ff5a356dc3620698e0a4b86
Alessio <alex.robassomero@gmail.com>
2021-10-01 10:46:51 +0200

    Convert StringView to pointer + size instead of pointer + pointer.
    Before StringView was a pointer to the start and a pointer to the end of the string.
    Now it's a pointer to the start and a size, this is to avoid issues with Debug mode on MSVC.

--- File.h
+++ File.h
@@ -17,7 +17,7 @@ namespace ARLib {
         public:
         OpenFileError() = default;
         OpenFileError(String filename) : m_filename(move(filename)) {}
-        static inline String error = "Couldn't open file "_s;
+        constexpr static inline StringView error = "Couldn't open file "_sv;
         const String& filename() const { return m_filename; }
     };
 
--- StringView.h
+++ StringView.h
@@ -9,60 +9,60 @@ namespace ARLib {
     class StringView {
         char* m_start_mut = nullptr;
         const char* m_start = nullptr;
-        const char* m_end = nullptr;
+        size_t m_size = 0;
 
         public:
         static constexpr auto npos = String::npos;
         constexpr StringView() = default;
 
-        constexpr StringView(const char* begin, const char* end) : m_start(begin), m_end(end) {}
-        constexpr StringView(char* begin, const char* end) : m_start_mut(begin), m_start(begin), m_end(end) {}
+        constexpr StringView(const char* begin, const char* end) : m_start(begin), m_size(end - begin) {}
+        constexpr StringView(char* begin, const char* end) : m_start_mut(begin), m_start(begin), m_size(end - begin) {}
 
         template <typename T>
         requires IsSameV<T, const char*>
-        constexpr StringView(T buf, size_t size) : m_start(buf), m_end(buf + size) {}
+        constexpr StringView(T buf, size_t size) : m_start(buf), m_size(size) {}
 
         template <typename T>
         requires IsSameV<T, char*>
-        constexpr StringView(T buf, size_t size) : m_start_mut(buf), m_start(buf), m_end(buf + size) {}
+        constexpr StringView(T buf, size_t size) : m_start_mut(buf), m_start(buf), m_size(size) {}
 
         template <size_t N>
-        consteval explicit StringView(const char (&buf)[N]) : m_start(buf), m_end(buf + N - 1) {}
+        consteval explicit StringView(const char (&buf)[N]) : m_start(buf), m_size(N - 1) {}
 
         template <typename T>
         requires IsSameV<T, const char*>
-        explicit constexpr StringView(T buf) : m_start(buf) { m_end = buf + strlen(buf); }
+        explicit constexpr StringView(T buf) : m_start(buf) { m_size = strlen(buf); }
 
         template <typename T>
         requires IsSameV<T, char*>
-        explicit constexpr StringView(T buf) : m_start_mut(buf), m_start(buf) { m_end = buf + strlen(buf); }
+        explicit constexpr StringView(T buf) : m_start_mut(buf), m_start(buf) { m_size = strlen(buf); }
 
-        explicit StringView(const String& ref) : m_start(ref.data()) { m_end = m_start + ref.length(); }
+        explicit StringView(const String& ref) : m_start(ref.data()) { m_size = ref.length(); }
         explicit StringView(String& ref) : m_start_mut(ref.rawptr()), m_start(ref.data()) {
-            m_end = m_start + ref.length();
+            m_size = ref.length();
         }
         constexpr StringView(const StringView& view) = default;
         constexpr StringView(StringView&& view) noexcept {
             m_start_mut = view.m_start_mut;
             m_start = view.m_start;
-            m_end = view.m_end;
+            m_size = view.m_size;
             view.m_start = nullptr;
-            view.m_end = nullptr;
+            view.m_size = 0;
         }
         constexpr StringView& operator=(const StringView& view) noexcept {
             if (this == &view) return *this;
             m_start_mut = view.m_start_mut;
             m_start = view.m_start;
-            m_end = view.m_end;
+            m_size = view.m_size;
             return *this;
         }
         constexpr StringView& operator=(StringView&& view) noexcept {
             m_start_mut = view.m_start_mut;
             m_start = view.m_start;
-            m_end = view.m_end;
+            m_size = view.m_size;
             view.m_start_mut = nullptr;
             view.m_start = nullptr;
-            view.m_end = nullptr;
+            view.m_size = 0;
             return *this;
         }
 
@@ -111,21 +111,21 @@ namespace ARLib {
             }
         }
         void print_view() { printf("%.*s\n", size(), m_start); }
-        [[nodiscard]] constexpr size_t size() const { return static_cast<size_t>(m_end - m_start); }
-        [[nodiscard]] constexpr size_t length() const { return static_cast<size_t>(m_end - m_start); }
+        [[nodiscard]] constexpr size_t size() const { return m_size; }
+        [[nodiscard]] constexpr size_t length() const { return m_size; }
         [[nodiscard]] constexpr const char* data() const { return m_start; }
         [[nodiscard]] constexpr char* rawptr() {
             HARD_ASSERT(m_start_mut, "This stringview is not mutable")
             return m_start_mut;
         }
         [[nodiscard]] ConstIterator<char> begin() { return ConstIterator<char>{m_start}; }
-        [[nodiscard]] ConstIterator<char> end() { return ConstIterator<char>{m_end}; }
+        [[nodiscard]] ConstIterator<char> end() { return ConstIterator<char>{m_start + m_size}; }
         [[nodiscard]] String extract_string() const { return {m_start, length()}; }
         explicit operator String() const { return extract_string(); }
         constexpr StringView substringview(size_t first = 0, size_t last = npos) {
             size_t ssize = size();
             if (first > ssize) return {};
-            if (last > ssize) return StringView{m_start + first, m_end};
+            if (last > ssize) return StringView{m_start + first, m_size};
             if (m_start_mut) {
                 return StringView{m_start_mut + first, m_start + last};
             } else {
