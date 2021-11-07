826892c8276d025b8314d04b1fd0df1ebeceeef3
Alessio <alex.robassomero@gmail.com>
2021-11-07 17:09:11 +0100

    Fix typo.

--- XNative/chrono/xnative_chrono_windows.h
+++ XNative/chrono/xnative_chrono_windows.h
@@ -2,7 +2,7 @@
 #include "../../Compat.h"
 #ifdef WINDOWS
 #if not defined(CHRONO_INCLUDED__) and not defined(INCLUDED_FROM_OWN_CPP___)
-#error "Don't include the XNative files directly. Use Use Chrono.h"
+#error "Don't include the XNative files directly. Use Chrono.h"
 #endif
 
 namespace ARLib {
