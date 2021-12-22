ca4c76c0e03c4255ffc67dfb903bd8eb1b7a2294
Alessio <alex.robassomero@gmail.com>
2021-12-22 19:53:08 +0100

    Make most of Array's functions constexpr

--- Array.h
+++ Array.h
@@ -13,38 +13,42 @@ namespace ARLib {
         T _m_storage_[S];
 
         private:
-        void assert_size_(size_t index) const {
+        constexpr void assert_size_(size_t index) const {
             SOFT_ASSERT_FMT((index < S), "Index %lu out of bounds in array of size %lu", index, S)
         }
 
         public:
         constexpr size_t size() const { return S; }
 
-        T& index(size_t index) {
+        constexpr T& index(size_t index) {
             assert_size_(index);
             return _m_storage_[index];
         }
 
-        const T& index(size_t index) const {
+        constexpr const T& index(size_t index) const {
             assert_size_(index);
             return _m_storage_[index];
         }
 
-        T& operator[](size_t index) {
+        constexpr T& operator[](size_t index) {
             assert_size_(index);
             return _m_storage_[index];
         }
 
-        const T& operator[](size_t index) const {
+        constexpr const T& operator[](size_t index) const {
             assert_size_(index);
             return _m_storage_[index];
         }
 
-        Iterator<T> begin() { return Iterator<T>{PointerTraits<T*>::pointer_to(*_m_storage_)}; }
-        Iterator<T> end() { return Iterator<T>{PointerTraits<T*>::pointer_to(*_m_storage_) + S}; }
+        constexpr Iterator<T> begin() { return Iterator<T>{PointerTraits<T*>::pointer_to(*_m_storage_)}; }
+        constexpr Iterator<T> end() { return Iterator<T>{PointerTraits<T*>::pointer_to(*_m_storage_) + S}; }
 
-        ConstIterator<T> begin() const { return ConstIterator<T>{PointerTraits<const T*>::pointer_to(*_m_storage_)}; }
-        ConstIterator<T> end() const { return ConstIterator<T>{PointerTraits<const T*>::pointer_to(*_m_storage_) + S}; }
+        constexpr ConstIterator<T> begin() const {
+            return ConstIterator<T>{PointerTraits<const T*>::pointer_to(*_m_storage_)};
+        }
+        constexpr ConstIterator<T> end() const {
+            return ConstIterator<T>{PointerTraits<const T*>::pointer_to(*_m_storage_) + S};
+        }
     };
 
     template <typename First, typename... Rest>
