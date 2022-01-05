a28a6bedaf74b84b6e259685f8b20533ac081f05
Alessio <alex.robassomero@gmail.com>
2022-01-05 03:13:55 +0100

    Modify Playground/main.cpp to test for latest feature addition.

--- Playground/main.cpp
+++ Playground/main.cpp
@@ -1,22 +1,24 @@
+#include "../CharConv.h"
 #include "../Printer.h"
-#include "../BigInt.h"
-
+#include "../PriorityQueue.h"
 using namespace ARLib;
 
 int main() {
-    auto a = BigInt{"12389123908"_s};
-    auto b = BigInt{"-983458171238123"_s};
-    auto c = BigInt{"875679183741987"_s};
-    auto d = BigInt{"-1238767812763"_s};
-    auto s = BigInt{"123456"_s};
-    Printer::print("Multiplication test: {} / {}", s * s, BigInt{-1} + BigInt{1});
-    auto f = BigInt{2234};
-    auto g = BigInt{4321};
-    auto f2 = BigInt{"12345678123456789"_s};
-    HARD_ASSERT(!f2.fits(), "fits??");
-    auto g2 = BigInt{1234};
-    Printer::print("{} {}", g / f, f2 / g2);
-    // Printer::print("Subtraction test: {} {} {} {} {} {}", a - b, b - a, a - c, c - a, b - d, d - b);
-    // Printer::print("Sumation test: {} {} {} {} {} {}", a + b, b + a, a + c, c + a, b + d, d + b);
+    auto queue_priority = [](const auto& left, const auto& right) {
+        if (left.size() > right.size())
+            return greater;
+        else if (left.size() < right.size())
+            return less;
+        return equal;
+    };
+    PriorityQueue<String, String> queue{queue_priority};
+    PriorityQueue<String> queue2{};
+    queue.push("123"_s);
+    queue.push("1234"_s);
+    queue2.push("hello"_s, 10);
+    queue2.push("world"_s, 20);
+    Printer::print("{} {}", queue2, queue);
+    Printer::print("{} {}", queue.pop(), queue2.pop());
+    Printer::print("{} {}", queue.size(), queue2.size());
     return 0;
 }
