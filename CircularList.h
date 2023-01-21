ec16294ac8ad0eaadfc1113d85c504281c6d7407
Alessio Rosiello <alex.robassomero@gmail.com>
2023-01-21 10:51:49 +0100

    Convert everything to UTF-8

--- CircularList.h
+++ CircularList.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 #include "Concepts.h"
 #include "Utility.h"
 namespace ARLib {
--- convert_to_utf8.ps1
+++ convert_to_utf8.ps1
@@ -1,10 +1,34 @@
-foreach($i in Get-ChildItem -filter "*.cpp") {
-    $temp = Get-Content $i.fullname
-    echo $temp
-    # Out-File -filepath $i.fullname -inputobject $temp -encoding utf8 -force
+function ConvertTo-UTF8 {
+    param(
+        [string]$path = $pwd, 
+        [bool]$recurse = $false
+    )
+    if ($recurse) {
+        foreach($i in Get-ChildItem -Recurse -Path $path -Filter "*.cpp") {
+            $temp = Get-Content $i.fullname
+            Out-File -filepath $i.fullname -inputobject $temp -encoding utf8 -force
+        }
+        foreach($i in Get-ChildItem -Recurse -Path $path -Filter "*.h") {
+            $temp = Get-Content $i.fullname
+            Out-File -filepath $i.fullname -inputobject $temp -encoding utf8 -force
+        }
+    } else {
+        foreach($i in Get-ChildItem -Path $path -Filter "*.cpp") {
+            $temp = Get-Content $i.fullname
+            Out-File -filepath $i.fullname -inputobject $temp -encoding utf8 -force
+        }
+        foreach($i in Get-ChildItem -Path $path -Filter "*.h") {
+            $temp = Get-Content $i.fullname
+            Out-File -filepath $i.fullname -inputobject $temp -encoding utf8 -force
+        }
+    }
 }
-foreach($i in Get-ChildItem -filter "*.h") {
-    $temp = Get-Content $i.fullname
-    echo $temp
-    # Out-File -filepath $i.fullname -inputobject $temp -encoding utf8 -force
-}
+ConvertTo-UTF8
+ConvertTo-UTF8 "ASM_GCC"
+ConvertTo-UTF8 "ASM_MSVC"
+ConvertTo-UTF8 "PlayGround"
+ConvertTo-UTF8 "Linux" $true
+ConvertTo-UTF8 "Windows" $true
+ConvertTo-UTF8 "XNative" $true
+ConvertTo-UTF8 "Tests"
+ConvertTo-UTF8 "Benchmark"
