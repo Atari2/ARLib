ec255b981ad37d6a3bc20efb8fca82ffb74e69e8
Alessio Rosiello <alex.robassomero@gmail.com>
2023-02-09 18:04:09 +0100

    Add more natvis types.

--- NatvisCompile.cpp
+++ NatvisCompile.cpp
@@ -2,6 +2,43 @@
 #ifdef COMPILER_MSVC
 namespace ARLib {
 	// this file exists just to force re-compilation upon changing the natvis file. 
-
+	// ARLib provides natvis support for the following classes
+	// - String
+	// - StringView
+	// - WString
+	// - WStringView
+	// - Path
+	// - Pair
+	// - UniquePtr
+	// - SharedPtr
+	// - Vector
+	// - Result
+	// - Optional
+	// - Array
+	// - Monostate
+	// - Variant
+	// - RefBox
+	
+	// currently not supported but on TODO list:
+	// - HashMap
+	// - Map
+	// - List
+	// - Tuple
+	// - {Reverse}{Const}Iterator
+	// - GenericView
+	// - IteratorView
+	// - Matrix
+	// - Stack
+	// - SortedVector
+	// - SSOVector
+	// - Tree
+	// - WeakPtr
+	// - SourceLocation
+	// - StackTrace
+	// - Mutex
+	// - UniqueLock
+	// - ScopedLock
+	// - LockGuard
+	// - Thread
 }
 #endif
