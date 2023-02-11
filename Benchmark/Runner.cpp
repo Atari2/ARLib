c88d1116bacb001c33305774f879eadbd89471ed
Alessio Rosiello <alex.robassomero@gmail.com>
2023-02-11 17:25:22 +0100

    format

--- Benchmark/Runner.cpp
+++ Benchmark/Runner.cpp
@@ -47,23 +47,21 @@ constexpr static const Array strings{
     "M798JiFBuA"_sv, "Sq51RKKFUZ"_sv, "6Lg6W9hZ9X"_sv, "HA741LK6Ai"_sv,
 };
 [[maybe_unused]] constexpr static const Array stdstrings{
-    "XZk6g68IJe"sv, "xLX1I3D9Ju"sv, "c8iChK6P5U"sv, "ke1fjfqM4h"sv, "C14RwSqFaK"sv, "ZYApUXmw8i"sv,
-    "NSsOfFyYQw"sv, "2embvf20ZJ"sv, "QXreMhn9Rk"sv, "OyTfRkPoWP"sv, "kHXnimdjGb"sv, "mtylTdDHs8"sv,
-    "HjDNsHZJ0O"sv, "o0MmzO0OQ7"sv, "C81lYiEoPK"sv, "fxpSWLS0in"sv, "QWLODBo9lj"sv, "8N0MIpeP8j"sv,
-    "3xbvOimAaK"sv, "wj41wl0ifb"sv, "vjHFCWCKU1"sv, "wQHSdcKmrn"sv, "PXTMNP75hq"sv, "L7TuxocgEG"sv,
-    "g7bQbXwCwG"sv, "OTgkPNbdRG"sv, "GXVpodrMcv"sv, "uXsiZQv2KK"sv, "144IRet2MM"sv, "eQWNvRkaFh"sv,
-    "PDggPLAcQD"sv, "WV4gj6eVx3"sv, "H2az78ED8Q"sv, "G3BKZOi5AP"sv, "00eUWw5keT"sv, "EekC8u3Fgp"sv,
-    "zBdebgoSjW"sv, "NpetUuoijR"sv, "hdNt1J3d35"sv, "Kyi8aIHEeo"sv, "dR0WL76qoO"sv, "ggrxoFagtP"sv,
-    "qDJN7Clkcd"sv, "8xPcyvzstU"sv, "Fp0V3JBAQ1"sv, "CAAIyvZJ2l"sv, "jHFIKEqoDP"sv, "0ePRmgMGJj"sv,
-    "UlCy28HaHT"sv, "ULgHyVJVX6"sv, "cG624ueEoO"sv, "A1hg0xHLfQ"sv, "C2kkYZkTtF"sv, "1yUtwevLgb"sv,
-    "za9G6Ry7RA"sv, "Kkfk6ZTfse"sv, "20y9tEFtv7"sv, "jbg7UzBTE1"sv, "qjlGhPxKn0"sv, "PgNHVLj5GD"sv,
-    "2I6GpgwhEj"sv, "4hVs495EX4"sv, "XdxSo7jBgU"sv, "inlBP3ELRI"sv, "h3mUk3Cw33"sv, "4rHb12uhoa"sv,
-    "DrycQ6SL94"sv, "GglNpPbzWw"sv, "J9gtEsYZ48"sv, "MpXA02CaUr"sv, "XwTX7hCsA0"sv, "p7USMCQ140"sv,
-    "yk14lFbf6S"sv, "OxPYxbX4Up"sv, "RRkhyOXK6j"sv, "6guDMubTGf"sv, "xdgN9VZ5xJ"sv, "sZVJ6V5bzs"sv,
-    "mlBklidD2m"sv, "vTPq38FlDd"sv, "KC7cKE88kp"sv, "CDtuCg6FoP"sv, "pJUrm9kB7t"sv, "l493YSmNkc"sv,
-    "oTI3T4gRRt"sv, "qZLEQQYxor"sv, "sRnv5wXIqn"sv, "VTZRkswfh7"sv, "q69Vq4wpIK"sv, "SWQ6dU4wLs"sv,
-    "nHTVA6jzsS"sv, "2phWwNfCHp"sv, "HQ6oNmPko5"sv, "LgUFsTN0eC"sv, "hlDB7lRGJ0"sv, "8kGMMJV7PR"sv,
-    "M798JiFBuA"sv, "Sq51RKKFUZ"sv, "6Lg6W9hZ9X"sv, "HA741LK6Ai"sv,
+    "XZk6g68IJe"sv, "xLX1I3D9Ju"sv, "c8iChK6P5U"sv, "ke1fjfqM4h"sv, "C14RwSqFaK"sv, "ZYApUXmw8i"sv, "NSsOfFyYQw"sv,
+    "2embvf20ZJ"sv, "QXreMhn9Rk"sv, "OyTfRkPoWP"sv, "kHXnimdjGb"sv, "mtylTdDHs8"sv, "HjDNsHZJ0O"sv, "o0MmzO0OQ7"sv,
+    "C81lYiEoPK"sv, "fxpSWLS0in"sv, "QWLODBo9lj"sv, "8N0MIpeP8j"sv, "3xbvOimAaK"sv, "wj41wl0ifb"sv, "vjHFCWCKU1"sv,
+    "wQHSdcKmrn"sv, "PXTMNP75hq"sv, "L7TuxocgEG"sv, "g7bQbXwCwG"sv, "OTgkPNbdRG"sv, "GXVpodrMcv"sv, "uXsiZQv2KK"sv,
+    "144IRet2MM"sv, "eQWNvRkaFh"sv, "PDggPLAcQD"sv, "WV4gj6eVx3"sv, "H2az78ED8Q"sv, "G3BKZOi5AP"sv, "00eUWw5keT"sv,
+    "EekC8u3Fgp"sv, "zBdebgoSjW"sv, "NpetUuoijR"sv, "hdNt1J3d35"sv, "Kyi8aIHEeo"sv, "dR0WL76qoO"sv, "ggrxoFagtP"sv,
+    "qDJN7Clkcd"sv, "8xPcyvzstU"sv, "Fp0V3JBAQ1"sv, "CAAIyvZJ2l"sv, "jHFIKEqoDP"sv, "0ePRmgMGJj"sv, "UlCy28HaHT"sv,
+    "ULgHyVJVX6"sv, "cG624ueEoO"sv, "A1hg0xHLfQ"sv, "C2kkYZkTtF"sv, "1yUtwevLgb"sv, "za9G6Ry7RA"sv, "Kkfk6ZTfse"sv,
+    "20y9tEFtv7"sv, "jbg7UzBTE1"sv, "qjlGhPxKn0"sv, "PgNHVLj5GD"sv, "2I6GpgwhEj"sv, "4hVs495EX4"sv, "XdxSo7jBgU"sv,
+    "inlBP3ELRI"sv, "h3mUk3Cw33"sv, "4rHb12uhoa"sv, "DrycQ6SL94"sv, "GglNpPbzWw"sv, "J9gtEsYZ48"sv, "MpXA02CaUr"sv,
+    "XwTX7hCsA0"sv, "p7USMCQ140"sv, "yk14lFbf6S"sv, "OxPYxbX4Up"sv, "RRkhyOXK6j"sv, "6guDMubTGf"sv, "xdgN9VZ5xJ"sv,
+    "sZVJ6V5bzs"sv, "mlBklidD2m"sv, "vTPq38FlDd"sv, "KC7cKE88kp"sv, "CDtuCg6FoP"sv, "pJUrm9kB7t"sv, "l493YSmNkc"sv,
+    "oTI3T4gRRt"sv, "qZLEQQYxor"sv, "sRnv5wXIqn"sv, "VTZRkswfh7"sv, "q69Vq4wpIK"sv, "SWQ6dU4wLs"sv, "nHTVA6jzsS"sv,
+    "2phWwNfCHp"sv, "HQ6oNmPko5"sv, "LgUFsTN0eC"sv, "hlDB7lRGJ0"sv, "8kGMMJV7PR"sv, "M798JiFBuA"sv, "Sq51RKKFUZ"sv,
+    "6Lg6W9hZ9X"sv, "HA741LK6Ai"sv,
 };
 static void BM_StdSprintf(benchmark::State& state) {
     char buf[1024]{};
@@ -76,7 +74,6 @@ static void BM_StdSprintf(benchmark::State& state) {
         benchmark::ClobberMemory();
     }
 }
-
 static void BM_StdUnorderedMap(benchmark::State& state) {
     std::unordered_map<StringView, int, Hash<StringView>> stdmap{};
     for (auto _ : state) {
@@ -91,9 +88,7 @@ static void BM_StdUnorderedMap(benchmark::State& state) {
             }
         }
         if (stdmap.size() != strings.size()) { ASSERT_NOT_REACHED("Map size is wrong") }
-        for (const auto& s : strings) {
-            stdmap.erase(s);
-        }
+        for (const auto& s : strings) { stdmap.erase(s); }
         if (stdmap.size() != 0) { ASSERT_NOT_REACHED("Map size is wrong") }
     }
 }
@@ -111,9 +106,7 @@ static void BM_ARLibHashMap(benchmark::State& state) {
             }
         }
         if (map.size() != strings.size()) { ASSERT_NOT_REACHED("Map size is wrong") }
-        for (const auto& s : strings) {
-            map.remove(s);
-        }
+        for (const auto& s : strings) { map.remove(s); }
         if (map.size() != 0) { ASSERT_NOT_REACHED("Map size is wrong") }
     }
 }
