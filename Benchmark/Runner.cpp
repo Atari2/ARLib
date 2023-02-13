5b5f1a88e95658c5e22a7b079b0fac42c94b718f
Alessio Rosiello <alex.robassomero@gmail.com>
2023-02-13 22:21:50 +0100

    Add CMakePresets.json

--- Benchmark/Runner.cpp
+++ Benchmark/Runner.cpp
@@ -20,10 +20,21 @@ static void BM_ARLibSprintf(benchmark::State& state) {
     char buf[1024]{};
     for (auto _ : state) {
         double val              = 1234.1234;
-        const char* s           = "my name is alessio";
+        const char* s           = "my name is anon";
         int hex                 = 0x50;
         ARLib::int64_t int64val = INT64_MAX;
         ARLib::sprintf(buf, "Hello World %+10.4f %% %s %#02o %#02I64X %%\n", val, s, hex, int64val);
+        benchmark::ClobberMemory();
+    }
+}
+static void BM_StdSprintf(benchmark::State& state) {
+    char buf[1024]{};
+    for (auto _ : state) {
+        double val              = 1234.1234;
+        const char* s           = "my name is anon";
+        int hex                 = 0x50;
+        ARLib::int64_t int64val = INT64_MAX;
+        ::sprintf(buf, "Hello World %+10.4f %% %s %#02o %#02" PRIX64 " %%\n", val, s, hex, int64val);
         benchmark::ClobberMemory();
     }
 }
@@ -46,34 +57,6 @@ constexpr static const Array strings{
     "nHTVA6jzsS"_sv, "2phWwNfCHp"_sv, "HQ6oNmPko5"_sv, "LgUFsTN0eC"_sv, "hlDB7lRGJ0"_sv, "8kGMMJV7PR"_sv,
     "M798JiFBuA"_sv, "Sq51RKKFUZ"_sv, "6Lg6W9hZ9X"_sv, "HA741LK6Ai"_sv,
 };
-[[maybe_unused]] constexpr static const Array stdstrings{
-    "XZk6g68IJe"sv, "xLX1I3D9Ju"sv, "c8iChK6P5U"sv, "ke1fjfqM4h"sv, "C14RwSqFaK"sv, "ZYApUXmw8i"sv, "NSsOfFyYQw"sv,
-    "2embvf20ZJ"sv, "QXreMhn9Rk"sv, "OyTfRkPoWP"sv, "kHXnimdjGb"sv, "mtylTdDHs8"sv, "HjDNsHZJ0O"sv, "o0MmzO0OQ7"sv,
-    "C81lYiEoPK"sv, "fxpSWLS0in"sv, "QWLODBo9lj"sv, "8N0MIpeP8j"sv, "3xbvOimAaK"sv, "wj41wl0ifb"sv, "vjHFCWCKU1"sv,
-    "wQHSdcKmrn"sv, "PXTMNP75hq"sv, "L7TuxocgEG"sv, "g7bQbXwCwG"sv, "OTgkPNbdRG"sv, "GXVpodrMcv"sv, "uXsiZQv2KK"sv,
-    "144IRet2MM"sv, "eQWNvRkaFh"sv, "PDggPLAcQD"sv, "WV4gj6eVx3"sv, "H2az78ED8Q"sv, "G3BKZOi5AP"sv, "00eUWw5keT"sv,
-    "EekC8u3Fgp"sv, "zBdebgoSjW"sv, "NpetUuoijR"sv, "hdNt1J3d35"sv, "Kyi8aIHEeo"sv, "dR0WL76qoO"sv, "ggrxoFagtP"sv,
-    "qDJN7Clkcd"sv, "8xPcyvzstU"sv, "Fp0V3JBAQ1"sv, "CAAIyvZJ2l"sv, "jHFIKEqoDP"sv, "0ePRmgMGJj"sv, "UlCy28HaHT"sv,
-    "ULgHyVJVX6"sv, "cG624ueEoO"sv, "A1hg0xHLfQ"sv, "C2kkYZkTtF"sv, "1yUtwevLgb"sv, "za9G6Ry7RA"sv, "Kkfk6ZTfse"sv,
-    "20y9tEFtv7"sv, "jbg7UzBTE1"sv, "qjlGhPxKn0"sv, "PgNHVLj5GD"sv, "2I6GpgwhEj"sv, "4hVs495EX4"sv, "XdxSo7jBgU"sv,
-    "inlBP3ELRI"sv, "h3mUk3Cw33"sv, "4rHb12uhoa"sv, "DrycQ6SL94"sv, "GglNpPbzWw"sv, "J9gtEsYZ48"sv, "MpXA02CaUr"sv,
-    "XwTX7hCsA0"sv, "p7USMCQ140"sv, "yk14lFbf6S"sv, "OxPYxbX4Up"sv, "RRkhyOXK6j"sv, "6guDMubTGf"sv, "xdgN9VZ5xJ"sv,
-    "sZVJ6V5bzs"sv, "mlBklidD2m"sv, "vTPq38FlDd"sv, "KC7cKE88kp"sv, "CDtuCg6FoP"sv, "pJUrm9kB7t"sv, "l493YSmNkc"sv,
-    "oTI3T4gRRt"sv, "qZLEQQYxor"sv, "sRnv5wXIqn"sv, "VTZRkswfh7"sv, "q69Vq4wpIK"sv, "SWQ6dU4wLs"sv, "nHTVA6jzsS"sv,
-    "2phWwNfCHp"sv, "HQ6oNmPko5"sv, "LgUFsTN0eC"sv, "hlDB7lRGJ0"sv, "8kGMMJV7PR"sv, "M798JiFBuA"sv, "Sq51RKKFUZ"sv,
-    "6Lg6W9hZ9X"sv, "HA741LK6Ai"sv,
-};
-static void BM_StdSprintf(benchmark::State& state) {
-    char buf[1024]{};
-    for (auto _ : state) {
-        double val              = 1234.1234;
-        const char* s           = "my name is alessio";
-        int hex                 = 0x50;
-        ARLib::int64_t int64val = INT64_MAX;
-        ::sprintf(buf, "Hello World %+10.4f %% %s %#02o %#02" PRIX64 " %%\n", val, s, hex, int64val);
-        benchmark::ClobberMemory();
-    }
-}
 static void BM_StdUnorderedMap(benchmark::State& state) {
     std::unordered_map<StringView, int, Hash<StringView>> stdmap{};
     for (auto _ : state) {
