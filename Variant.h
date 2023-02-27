6a3743fa4249cc02ac308c84dab3797c5e15b8e4
Alessio Rosiello <alex.robassomero@gmail.com>
2023-02-27 18:45:00 +0100

    change placement new to just assignment.

--- Variant.h
+++ Variant.h
@@ -226,20 +226,19 @@ namespace detail {
             : head(move(args)...), is_active(true) {}
         VariantStorage() : is_active(false) {}
         VariantStorage(VariantStorage&& other) noexcept : is_active(other.is_active) {
-            if (other.is_active) new (&head) Type(move(other.head));
+            if (other.is_active) head = move(other.head);
             is_active = other.is_active;
         }
         VariantStorage(const VariantStorage& other) : is_active(other.is_active) {
-            if (other.is_active) new (&head) Type(other.head);
-            is_active = other.is_active;
+            if (other.is_active) head = other.head;
         }
         VariantStorage& operator=(VariantStorage&& other) noexcept {
-            if (other.is_active) new (&head) Type(move(other.head));
+            if (other.is_active) head = move(other.head);
             is_active = other.is_active;
             return *this;
         }
         VariantStorage& operator=(const VariantStorage& other) {
-            if (other.is_active) new (&head) Type(other.head);
+            if (other.is_active) head = other.head;
             is_active = other.is_active;
             return *this;
         }
