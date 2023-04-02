931d491ce193805b9872144f36e0fb6a7d84a3e6
Alessio Rosiello <alex.robassomero@gmail.com>
2023-04-02 11:21:17 +0200

    Fix playground.

--- Playground/main.cpp
+++ Playground/main.cpp
@@ -4,37 +4,70 @@
 #include "../Printer.h"
 #include "../File.h"
 #include "../JSONParser.h"
+#include "../FlatMap.h"
+#include "../FlatSet.h"
+#include "../HashTable.h"
 using namespace ARLib;
-struct TestJson {
-    int val;
-    String str;
-    static JSON::Parsed<TestJson> deserialize(StringView string) {
-        auto obj = JSON::Parser::parse(string);
-        if (obj.is_error()) return obj.to_error();
-        auto jval = obj.to_ok();
-        TestJson val{ .val = jval["val"_s].get<JSON::Type::JNumber>(),
-                      .str = jval["str"_s].get<JSON::Type::JString>() };
-        return val;
+
+constexpr static size_t n_of_strings = 400;
+void fill_set(FlatSet<String>& set) {
+    for (size_t i = 1; i < n_of_strings; ++i) {
+        String s{ i, 'a' };
+        set.insert(move(s));
     }
-    String serialize() const { 
-        JSON::Object obj;
-        obj.add("val"_s, JSON::Number{val});
-        obj.add("str"_s, JSON::JString{ str });
-        return JSON::dump_json(obj);
+}
+void search_set(const FlatSet<String>& set) {
+    for (size_t i = 1; i < n_of_strings; ++i) {
+        String s{ i, 'a' };
+        Printer::print("len {} was {}", s.size(), set.find(s) != set.end() ? "found" : "not found");
     }
-};
-int main(int argc, char** argv) {
-    Result r{ 1 };
-    Result<int> r2{ BacktraceError{ "hello"_s } };
-    HARD_ASSERT(r.is_ok(), "1");
-    HARD_ASSERT(!r.is_error(), "2");
-    HARD_ASSERT(r.ok_value() == 1, "3");
-    ArgParser parser{ argc, argv };
-    String filename;
-    parser.add_option("--file", "FILE", "File to pass", filename);
-    if (auto res = parser.parse(); res.is_error()) { Printer::print("{}", res.to_error()); }
-    if (parser.help_requested()) { parser.print_help(); }
-    auto j = R"({"hello world": 1})"_json;
-    auto obj = MUST(TestJson::deserialize(R"({"val": 10, "str": "Hello World!"})"_sv));
-    Printer::print("{}", obj.serialize());
+}
+void erase_all(FlatSet<String>& set) {
+    for (size_t i = 1; i < n_of_strings; ++i) {
+        String s{ i, 'a' };
+        Printer::print("len {} was {}", s.size(), set.remove(s) ? "removed" : "not removed");
+    }
+}
+
+void fill_map(FlatMap<String, int>& set) {
+    for (size_t i = 1; i < n_of_strings; ++i) {
+        String s{ i, 'a' };
+        set.insert(move(s), i);
+    }
+}
+void search_map(const FlatMap<String, int>& set) {
+    for (size_t i = 1; i < n_of_strings; ++i) {
+        String s{ i, 'a' };
+        Printer::print("len {} was {}", s.size(), set.find(s) != set.end() ? "found" : "not found");
+    }
+}
+void erase_map(FlatMap<String, int>& set) {
+    for (size_t i = 1; i < n_of_strings; ++i) {
+        String s{ i, 'a' };
+        Printer::print("len {} was {}", s.size(), set.remove(s) ? "removed" : "not removed");
+    }
+}
+
+int main() {
+    FlatSet<String> set{};
+    fill_set(set);
+    search_set(set);
+    for (const auto& str : set) { Printer::print("{}", str); }
+    erase_all(set);
+    for (const auto& str : set) { Printer::print("{}", str); }
+
+
+    FlatMap<StringView, int> map2{};
+    map2.insert("hello"_sv, 1);
+    map2.find("hello");
+    
+    
+    FlatMap<String, int> map{};
+    fill_map(map);
+    search_map(map);
+    map.remove("aaaa"_s);
+    for (const auto& [k, v] : map) { Printer::print("{} {}", k, v); }
+    erase_map(map);
+    for (const auto& [k, v] : map) { Printer::print("{} {}", k, v); }
+    return 0;
 }
