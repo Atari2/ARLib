c0c5018ded997299eea5e674c656af000afd6bab
Alessio Rosiello <alex.robassomero@gmail.com>
2023-06-29 21:25:53 +0200

    Add Timer class

--- include/Chrono.hpp
+++ include/Chrono.hpp
@@ -12,4 +12,18 @@ class DateClock {
     static Instant now() { return ChronoNative::datenow(); }
     static Duration diff(Instant t1, Instant t2) { return Duration{ Nanos{ t2.raw_value() - t1.raw_value() } }; }
 };
+class Timer {
+    Instant m_start;
+    Instant m_last;
+    public:
+    Timer() : m_start{ PerfClock::now() }, m_last{ m_start } {}
+    void reset() { m_start = PerfClock::now(); }
+    Duration elapsed() const { return PerfClock::diff(m_start, PerfClock::now()); }
+    Duration elapsed_since_last() {
+		auto now = PerfClock::now();
+		auto diff = PerfClock::diff(m_last, now);
+		m_last = now;
+		return diff;
+	}
+};
 }    // namespace ARLib
