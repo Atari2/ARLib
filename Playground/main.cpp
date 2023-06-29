baf4546c32f247540c9c30e08baf6a89e2bc82a7
Alessio Rosiello <alex.robassomero@gmail.com>
2023-06-29 22:48:53 +0200

    Change playground.

--- Playground/main.cpp
+++ Playground/main.cpp
@@ -4,56 +4,31 @@
 #include "Array.hpp"
 #include "Printer.hpp"
 #include "CharConv.hpp"
-#include "JSONObject.hpp"
+#include "JSONParser.hpp"
+#include "FileSystem.hpp"
 using namespace ARLib;
-
 int main() {
-    /*
-    auto oldest_date = Date{ Instant::from_nanos(0_ns), Date::Timezone::No };
-    Printer::print("{}", oldest_date);
-
-    auto path = windows_build ? R"(C:\Users\aless\source\repos\ARLib\source)"_p : R"(/home/alessio/.vs/ARLib/source)"_p;
-
-    auto cont = DirectoryIterate{ path, true };
-
-    Vector<FileInfo> entries{};
-
-    for (const auto& entry : cont) {
-        entries.append(entry);
-        if (entry.is_directory()) {
-            Printer::print("{} is a directory", entry.path());
-        } else {
-            Printer::print("{} is a file", entry.path());
-        }
-    }
-
-    sort(entries, [](const FileInfo& a, const FileInfo& b) { return a.last_modification() <=> b.last_modification(); });
-
-    for (const auto& entry : entries) { Printer::print("Last modification at: {}", Date{ entry.last_modification() }); }
-    */
-
-    v2::Variant<int, double, Vector<int>, String, Pair<int, double>> t1{
-        "Hello world this is a long string that should be freed"
-    };
-    constexpr size_t s = sizeof(t1);
-    auto t2            = t1;
-    const auto& v      = t1.get<String>();
-    Printer::print("{}", v);
-    t1 = 1;
-    t1.set(Vector{ 1, 2, 3 });
-    Printer::print("{}", t1.current_type());
-    t1.visit([](const auto& asdf) { Printer::print("{}", asdf); });
-    Printer::print("t2: {}", t2);
-    /*
-    constexpr Array sizes{ sizeof(int), sizeof(double), sizeof(Vector<int>), sizeof(String) };
-
-    constexpr auto max_size = *max(sizes);
-
-    constexpr auto total = sum(sizes, [](size_t a) { return a;  });
-
-    Variant<int, double, Vector<int>, String> t2{};
-    constexpr size_t t1size = sizeof(t1);
-    constexpr size_t t2size = sizeof(t2);
-    */
+    const auto path = windows_build ? R"(C:\Users\aless\Downloads\package)"_p : R"(/home/alessio/package)"_p;
+    Timer c{};
+    auto data = iterate_directory(path, DirIterType::Recursive)
+                .iter()
+                .filter([](const FileInfo& info) { return info.is_file() && info.path().extension() == ".json"_p; })
+                .map([](const FileInfo& p) {
+                    const auto str = MUST(File::read_all(p.path()));
+                    auto res       = JSON::Parser::parse(str.view());
+                    if (res.is_error()) {
+                        auto err = res.to_error();
+                        auto j   = MUST(JSON::Parser::parse(str.substringview(0, err.offset())));
+                        return Pair{ FileInfo{ p }, j };
+                    }
+                    return Pair{ FileInfo{ p }, res.to_ok() };
+                })
+                .collect<Vector>();
+    sort(data, [](const auto& lhs, const auto& rhs) { return rhs.first().filesize() <=> lhs.first().filesize(); });
+    const auto total = data.iter().map([](const auto& p) { return p.first().filesize(); }).sum();
+    Printer::print(
+    "Parsed {} (total of {} GB) json files in {}", data.size(), (double)total / (double)(1_u64 << 30),
+    c.elapsed().to_common().millis()
+    );
     return 0;
 }
