6edeebf579f6f604723376ac8b606d72b818a14e
Alessio Rosiello <alex.robassomero@gmail.com>
2023-07-12 22:02:12 +0200

    Improve Console::getnumber

--- include/Console.hpp
+++ include/Console.hpp
@@ -18,15 +18,61 @@ struct Console {
         if (buf.back() == '\r') { buf.set_size(buf.size() - 1); }
         return buf;
     }
-    static Result<int> getint(int base = 10) {
+    template <typename Type>
+    requires(Integral<Type> || FloatingPoint<Type>)
+    static size_t get_max_len_for_type(int base) {
+        using NTArray =
+        TypeArray<int8_t, uint8_t, int16_t, uint16_t, int32_t, uint32_t, int64_t, uint64_t, float, double>;
+        constexpr size_t base_10_index = 0;
+        constexpr size_t base_2_index  = 1;
+        constexpr size_t base_8_index  = 2;
+        constexpr size_t base_16_index = 3;
+
+        constexpr size_t typeindex = NTArray::IndexOf<Type>;
+
+        constexpr size_t SSOCAP = /* String::SMALL_STRING_CAP */ 15;
+
+        constexpr size_t size_matrix[4][10] = {
+            { 3, 3, 5, 5, 10, 10, 19, 20, 46, 316 }, // base 10
+            { 8, 8, 16, 16, 32, 32, 64, 64, 32, 64 }, // base 2
+            { 3, 3, 5, 6, 11, 11, 21, 22, SSOCAP, SSOCAP }, // base 8
+            { 2, 2, 4, 4, 8, 8, 16, 16, SSOCAP, SSOCAP }  // base 16
+        };
+
+        switch (base) {
+            case 10:
+                return size_matrix[base_10_index][typeindex];
+            case 2:
+                return size_matrix[base_2_index][typeindex];
+            case 8:
+                return size_matrix[base_8_index][typeindex];
+            case 16:
+                return size_matrix[base_16_index][typeindex];
+            default:
+                return SSOCAP;
+        }
+    }
+    template <typename Type>
+    requires(Integral<Type> || FloatingPoint<Type>)
+    static Result<Type> getnumber(int base = 10) {
         String buf{};
+        buf.reserve(get_max_len_for_type<Type>(base));
         char c = static_cast<char>(ARLib::getchar());
         while (!isspace(c) && c != EOF) {
             buf += c;
             c = static_cast<char>(ARLib::getchar());
         }
-        if (buf.size() == 0) return 0;
-        return StrToInt(buf, base);
+        if (buf.size() == 0) return static_cast<Type>(0);
+        if constexpr (UnsignedIntegral<Type>) {
+            TRY_SET(t, StrToU64(buf, base));
+            return static_cast<Type>(t);
+        } else if constexpr (SignedIntegral<Type>) {
+            TRY_SET(t, StrToI64(buf, base));
+            return static_cast<Type>(t);
+        } else /* (FloatingPoint<Type>) */ {
+            TRY_SET(t, StrToDouble(buf));
+            return static_cast<Type>(t);
+        }
     }
     template <size_t N, typename... Args>
     static void print(const char (&fmt)[N], const Args&... args) {
