801e3ab6e537e939834dfecd9b665647de6be925
Alessio Rosiello <alex.robassomero@gmail.com>
2023-08-21 01:29:11 +0200

    Fixup CSVParser parse api

--- include/CSVParser.hpp
+++ include/CSVParser.hpp
@@ -5,16 +5,39 @@
 #include "File.hpp"
 #include "Result.hpp"
 namespace ARLib {
+struct CSVErrorInfo {
+    String error_string;
+    size_t error_offset;
+};
+class CSVParseError : public ErrorBase {
+    CSVErrorInfo m_info;
+
+    public:
+    CSVParseError() = default;
+    CSVParseError(String error, size_t offset) : m_info{ move(error), offset } {};
+    CSVParseError(CSVErrorInfo info) : m_info(move(info)){};
+    const CSVErrorInfo& info() const { return m_info; }
+    const String& message() const { return m_info.error_string; }
+    const String& error_string() const { return m_info.error_string; }
+    size_t offset() const { return m_info.error_offset; }
+};
+class CSVRow;
+class CSVHeader;
+using CSVRH = Variant<CSVRow, CSVHeader>;
+using CSVHeaderResult = Result<CSVHeader, CSVParseError>;
+using CSVRowResult = Result<CSVRow, CSVParseError>;
+using CSVResult = Result<CSVRH, CSVParseError>;
+
 class CSVHeader {
     friend class CSVParser;
     Vector<String> m_row;
-    static Result<CSVHeader> parse_header(String&& line);
+    static CSVHeaderResult parse_header(File& line);
     CSVHeader(Vector<String>&& row) : m_row(move(row)) {}
 };
 class CSVRow {
     friend class CSVParser;
     Vector<String> m_row;
-    static Result<CSVRow> parse_row(String&& line);
+    static CSVRowResult parse_row(File& line);
     CSVRow(Vector<String>&& row) : m_row(move(row)) {}
 };
 class CSVParser {
@@ -25,15 +48,13 @@ class CSVParser {
     bool m_has_header;
     bool m_read_header;
 
-    using CSVRH = Variant<CSVRow, CSVHeader>;
-
     public:
     CSVParser(Path p) :
         m_rows{}, m_header{}, m_file{ move(p) }, m_separator{ ',' }, m_has_header{ false }, m_read_header{ false } {}
     DiscardResult<FileError> open();
     void with_header(bool has_header) { m_has_header = move(has_header); }
     void with_separator(char separator) { m_separator = separator; }
-    Result<CSVRH> read_row();
-    Result<Vector<CSVRH>> read_all();
+    CSVResult read_row();
+    Result<Vector<CSVRH>, CSVParseError> read_all();
 };
 }    // namespace ARLib
