14d621250e89237a3dce95aae7515fd367148a2f
Alessio Rosiello <alex.robassomero@gmail.com>
2024-04-06 16:55:32 +0200

    Document allocator behavior

--- include/Allocator.hpp
+++ include/Allocator.hpp
@@ -86,6 +86,16 @@ void deallocate(T* allocated_ptr, SourceLocation LOC = SourceLocation::current()
         delete ptr;
     else { 
         void* mem = static_cast<void*>(ptr);
+        // this is a bit problematic
+        // because this means that every container that has called allocate_initialized or allocate_uninitialized
+        // has to destroy all the owned objects manually
+        // since this deallocate function only deallocates the memory but doesn't destroy the objects
+        // this is mostly because
+        // A - this function doesn't know **how many** objects there are to destroy, because this memory was allocated using ::operator new
+        // B - even if we did, we don't know how many of those are initialized and safe to call the dtor on, for all we know the caller may have initialized
+        //     objects at index 0 and 3, but not 1 and 2, and there is no way to know.
+        // tldr: all callers of deallocate<T, DeallocType::Multiple> **must** destroy their objects beforehand
+        //       by doing something like memory[0]->~T(), otherwise proper cleanup won't happen.
         ::operator delete(mem);
     }
 }
