a78c58dbd519d1b9e02c48aea75db7368e5050b3
Alessio <alex.robassomero@gmail.com>
2026-03-04 17:54:34 +0100

    Adjust testing to make it picked up by Visual Studio, add Optional variant with pointer specialization

--- include/Optional.hpp
+++ include/Optional.hpp
@@ -48,6 +48,37 @@ class OptionalStorage {
     ~OptionalStorage() { destroy(); }
 };
 template <typename T>
+class OptionalStorage<T*> {
+    T* m_value{ nullptr };
+    public:
+    OptionalStorage() = default;
+    OptionalStorage(const OptionalStorage& other) { m_value = other.m_value; }
+    OptionalStorage(OptionalStorage&& other) noexcept {
+        m_value       = other.m_value;
+        other.m_value = nullptr;
+    }
+    OptionalStorage& operator=(const OptionalStorage& other) {
+        m_value = other.m_value;
+        return *this;
+    }
+    OptionalStorage& operator=(OptionalStorage&& other) noexcept {
+        m_value       = other.m_value;
+        other.m_value = nullptr;
+        return *this;
+    }
+    void initialize(T* value) { m_value = value; }
+    const T* ptr() const { return m_value; }
+    T* ptr() { return m_value; }
+    bool exists() const { return m_value != nullptr; }
+    void destroy() { m_value = nullptr; }
+    T* extract() {
+        T* ret  = m_value;
+        m_value = nullptr;
+        return ret;
+    }
+    ~OptionalStorage() { destroy(); }
+};
+template <typename T>
 class OptionalStorage<T&> {
     T* m_value{ nullptr };
     public:
@@ -169,11 +200,10 @@ class Optional {
     AddConstT<Ret> operator*() const { return m_object.as(); }
     Ret operator*() { return m_object.as(); }
     T value_or(T&& default_value) &&
-    requires(!IsLvalueReferenceV<T>)
-    {
+    requires(!IsLvalueReferenceV<T>) {
         if (!m_object.exists()) return move(default_value);
         return m_object.extract();
-    }
+    } 
     T value_or(T default_value) &&
     requires IsLvalueReferenceV<T>
     {
@@ -211,6 +241,94 @@ class Optional {
     template <typename Func>
     Optional<InvokeResultT<Func, T>> map(Func func) && {
         using OutT = Optional<InvokeResultT<Func, T>>;
+        if (!m_object.exists()) {
+            return OutT{};
+        } else {
+            return OutT{ invoke(func, move(m_object.extract())) };
+        }
+    }
+    void evict() { m_object.destroy(); }
+    ~Optional() { evict(); }
+};
+template <typename T>
+class Optional<T*> {
+    OptionalStorage<T*> m_object;
+
+    using Ret   = ConditionalT<IsLvalueReferenceV<T>, T, AddLvalueReferenceT<T>>;
+    using CTR   = AddConstT<AddLvalueReferenceT<RemoveCvRefT<T>>>;
+    using BaseT = RemoveCvRefT<T>;
+
+    public:
+    Optional()                                 = default;
+    Optional(const Optional&)                  = default;
+    Optional(Optional&&)                       = default;
+    Optional& operator=(const Optional& other) = default;
+    Optional& operator=(Optional&& other)      = default;
+    Optional(T* val) { m_object.initialize(val); }
+    Optional& operator=(T* val) {
+        m_object.destroy();
+        m_object.initialize(val);
+        return *this;
+    }
+    bool operator==(const Optional& other) const
+    requires EqualityComparable<T>
+    {
+        // both are empty => true
+        if (!m_object.exists() && !other.m_object.exists()) return true;
+        // one of them is empty => false
+        if (!m_object.exists() || !other.m_object.exists()) return false;
+        return m_object.as() == other.m_object.as();
+    }
+    bool operator==(CTR other) const
+    requires EqualityComparable<BaseT>
+    {
+        if (!m_object.exists()) return false;
+        return m_object.as() == other;
+    }
+    Ordering operator<=>(const Optional& other) const {
+        // both are empty => true
+        if (!m_object.exists() && !other.m_object.exists()) return equal;
+        // one of them is empty => false
+        if (!m_object.exists()) return less;
+        if (!other.m_object.exists()) return greater;
+        if constexpr (Orderable<T>) {
+            return value() <=> other.value();
+        } else {
+            return CompareThreeWay(value(), other.value());
+        }
+    }
+    bool operator!() const { return !m_object.exists(); }
+    explicit operator bool() const { return m_object.exists(); }
+    bool empty() const { return !m_object.exists(); }
+    bool has_value() const { return m_object.exists(); };
+    AddConstT<Ret> value() const& { return m_object.as(); }
+    Ret value() & { return m_object.as(); }
+    T value() && { return m_object.extract(); }
+    const auto* operator->() const { return m_object.ptr(); }
+    auto* operator->() { return m_object.ptr(); }
+    AddConstT<Ret> operator*() const { return m_object.as(); }
+    Ret operator*() { return m_object.as(); }
+    T* value_or(T* default_value) {
+        if (!m_object.exists()) return default_value;
+        return m_object.extract();
+    }
+    void put(T* value) {
+        m_object.destroy();
+        m_object.initialize(T{ Forward<T>(value) });
+    }
+    T* extract() { return m_object.extract(); }
+    template <typename Func>
+    Optional<InvokeResultT<Func, T*>> map(Func func) & {
+        using OutT = Optional<InvokeResultT<Func, T*>>;
+        if (!m_object.exists()) {
+            return OutT{};
+        } else {
+            return OutT{ invoke(func, m_object.ptr()) };
+        }
+    }
+    template <typename Func>
+    Optional<InvokeResultT<Func, T*>> map(Func func) && {
+        using OutT = Optional<InvokeResultT<Func, T*>>;
         if (!m_object.exists()) {
             return OutT{};
         } else {
@@ -296,7 +414,8 @@ class FilterMapIterator {
     using InputValueType = IteratorInputType<IterUnit>;
     // this is necessary so that if we have a map iterator of an iterator that returns an lvalue we can return a copy of the output
     // instead of a reference to something that may be dead
-    using OutputValueType = ConditionalT<IsLvalueReferenceV<InternalType>, InternalType, AddLvalueReferenceT<InternalType>>;
+    using OutputValueType =
+    ConditionalT<IsLvalueReferenceV<InternalType>, InternalType, AddLvalueReferenceT<InternalType>>;
     FilterMapIterator(IterUnit unit, IterUnit end, Functor func, bool is_end = false) :
         m_current_iter(unit), m_end(end), m_func(func) {
         if (!is_end) { advance(); }
